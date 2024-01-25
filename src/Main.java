8c9f93d4039239f6ab031ae319e9d548355f7ba1
ACER <hongpink473@gmail.com>
2024-01-25 14:37:17 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import Person.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("MengLeang");
+        person.setAddress("Phnome Penh");
+        person.printPerson();
+    }
+}
---
+++ src/Person/Person.java
@@ -0,0 +1,36 @@
+package Person;
+
+public class Person {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    private String address;
+    public Person(){
+        this.address = "N/A";
+        this.name = "N/A";
+    }
+    public Person(String name, String address){
+        this.name = name;
+        this.address = address;
+    }
+
+    public void printPerson(){
+        System.out.println(getName() + " " + getAddress());
+    }
+
+}
